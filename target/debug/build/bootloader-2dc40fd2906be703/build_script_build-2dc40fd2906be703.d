364ae5e7208af94a9ed76e103eb3779bba39bdcb
malcomkamau <malcomkamau504@gmail.com>
2026-02-15 01:52:41 +0300

    feat: Initialize new Rust kernel project with basic build configuration.

---
+++ target/debug/build/bootloader-2dc40fd2906be703/build_script_build-2dc40fd2906be703.d
@@ -0,0 +1,5 @@
+/home/malcom/Documents/repositories/kernel-from-scratch/target/debug/build/bootloader-2dc40fd2906be703/build_script_build-2dc40fd2906be703.d: /home/malcom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/build.rs
+
+/home/malcom/Documents/repositories/kernel-from-scratch/target/debug/build/bootloader-2dc40fd2906be703/build_script_build-2dc40fd2906be703: /home/malcom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/build.rs
+
+/home/malcom/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/build.rs:
---
+++ target/debug/build/compiler_builtins-f6fb91fede64f633/build_script_build-f6fb91fede64f633.d
@@ -0,0 +1,6 @@
+/home/malcom/Documents/repositories/kernel-from-scratch/target/debug/build/compiler_builtins-f6fb91fede64f633/build_script_build-f6fb91fede64f633.d: /home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
+
+/home/malcom/Documents/repositories/kernel-from-scratch/target/debug/build/compiler_builtins-f6fb91fede64f633/build_script_build-f6fb91fede64f633: /home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
+
+/home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs:
+/home/malcom/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs:
